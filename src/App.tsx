a79a38ad17517a71299a51353f1a05702590f997
nguyenhai3112 <nguyenhai311294@gmail.com>
2024-07-09 00:51:07 +0700

    feat: Add side bả

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,54 @@
-import style from "./App.module.scss";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Layout, Menu } from "antd";
+import {
+  HomeOutlined,
+  ShoppingOutlined,
+  UploadOutlined,
+  PlusOutlined,
+} from "@ant-design/icons";
 import VocabularyStudying from "./component/vocabularyStudying/VocabularyStudying";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ShoppingComponent from "./component/shopping/ShoppingComponent";
 import ImageUpload from "./component/upload-image/UploadImage";
 import WordList from "./component/word-list/WordList";
-function App() {
+import style from "./App.module.scss";
+
+const { Sider, Content } = Layout;
+
+const App: React.FC = () => {
   return (
     <Router>
-      <div className={style.app}>
-        <Routes>
-          <Route path="/" element={<VocabularyStudying />} />
-          <Route path="/vocabulary" element={<VocabularyStudying />} />
-          <Route path="/shopping" element={<ShoppingComponent />} />
-          <Route path="/upload" element={<ImageUpload />} />
-          <Route path="/add-word" element={<WordList />} />
-        </Routes>
-      </div>
+      <Layout className={style.app}>
+        <Sider className={style.sidebar} breakpoint="lg" collapsedWidth="0">
+          <Menu mode="inline">
+            <Menu.Item key="1" icon={<HomeOutlined />}>
+              <Link to="/">Vocabulary Studying</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<ShoppingOutlined />}>
+              <Link to="/shopping">Shopping</Link>
+            </Menu.Item>
+            <Menu.Item key="4" icon={<UploadOutlined />}>
+              <Link to="/upload">Upload Image</Link>
+            </Menu.Item>
+            <Menu.Item key="5" icon={<PlusOutlined />}>
+              <Link to="/vocabulary-management">Vocabulary Management</Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className={style.siteLayout}>
+          <Content className={style.mainContent}>
+            <Routes>
+              <Route path="/" element={<VocabularyStudying />} />
+              <Route path="/vocabulary" element={<VocabularyStudying />} />
+              <Route path="/shopping" element={<ShoppingComponent />} />
+              <Route path="/upload" element={<ImageUpload />} />
+              <Route path="/vocabulary-management" element={<WordList />} />
+            </Routes>
+          </Content>
+        </Layout>
+      </Layout>
     </Router>
   );
-}
+};
 
 export default App;
